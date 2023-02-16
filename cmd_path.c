f40f1a56ba39699614007a3349d1d0c236a2ebaf
Gibexz <gibexzetedu@gmail.com>
2023-02-16 21:34:27 +0000

    More updates

--- cmd_path.c
+++ cmd_path.c
@@ -6,24 +6,28 @@
  *
  */
 char *cmd_check(char **args)
-{
+{/*
 	int i;
 	char *new_Path;
 
 	for (i = 0; i < strlen(args[0]); i++)
-	{
+	{*/ /*
 		if (args[0][i] != '/')
-		{
-			char new_args0[100] = "/bin/";
+		{*/
+		/*	char new_args0[100] = "/bin/"; */
 
-			strcat(new_args0, args[0]);
+		/*	strcat(new_args0, args[0]);
 
 			new_Path = malloc(strlen(new_args0) + 1);
 			strcpy(new_Path, new_args0);
 
 			return (new_Path);
-		}
+		}*//*
 		else
+		{
+			free(new_Path);
 			return (args[0]);
-	}
+		}
+	}*/
+
 }
