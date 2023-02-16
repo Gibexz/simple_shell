4962d762320b6b7fc4a857e62b45d1d418bee2bf
frekey20 <frekeycolz@gmail.com>
2023-02-16 14:22:41 -0800

    update

--- cmd_path.c
+++ cmd_path.c
@@ -30,4 +30,36 @@ char *cmd_check(char **args)
 		}
 	}*/
 
+
+	int i;
+	char *path = getenv("PATH");
+
+	for (i = 0; i < strlen(args[0]; i++)
+	{
+		if (arg[0][i] != '/')
+		{
+			char *path_dir = strtok(path, ":");
+
+			while (path_dir != NULL)
+			{
+				char *p_arg0 = malloc(strlen(path_dir) + strlen(args[0]) + 1);
+				if (p_arg0 == NULL)
+				{
+					perror(argv[0])
+					return(1);
+				}
+				sprintf (p_arg0, "%s/%s", path_dir, args[0]);
+				if (access(p_arg0, X_OK) == 0)
+				{
+					return (p_arg0);
+				}
+				free(p_arg0);
+				path_dir = strtok(NULL, ":");
+			}
+
+		}
+
+	}
+
+
 }
